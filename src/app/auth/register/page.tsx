aba6d66cc04cc4b4e98e5665a3ede60b040ccead
SqushiLoL <mariusgreiersen@gmail.com>
2024-06-07 20:37:30 +0200

    Register + Sign
    Translated to Danish, and fixed layout

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -6,185 +6,187 @@ import Link from "next/link";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Info } from "lucide-react";
 import Image from "next/image";
+import wave from "@/../public/wave.svg";
+import bankIcon from "@/../public/bank-icon.png";
 
 type Props = {};
 type LoginInput = {
-  email: string;
-  password: string;
-  first_name: string;
-  last_name: string;
+    email: string;
+    password: string;
+    first_name: string;
+    last_name: string;
 };
 
 export default function Register(props: Props) {
-  const [inputs, setInputs] = useState<LoginInput>({
-    email: "",
-    password: "",
-    first_name: "",
-    last_name: "",
-  });
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs((values) => ({ ...values, [name]: value }));
-  };
-
-  const { toast } = useToast();
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    const response = await fetch("/api/auth/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(inputs),
+    const [inputs, setInputs] = useState<LoginInput>({
+        email: "",
+        password: "",
+        first_name: "",
+        last_name: "",
     });
-    if (response.ok) {
-      toast({
-        title: "User created",
-        description: "User has been created successfully",
-      });
-      await signIn("credentials", {
-        email: inputs.email,
-        password: inputs.password,
-        callbackUrl: "/",
-      });
-    } else {
-      toast({
-        title: "Error",
-        description: "An error occurred trying to create the user",
-        variant: "destructive",
-      });
-    }
-  };
 
-  return (
-    <div className="min-h-screen flex">
-      <div className="flex flex-col justify-center items-center w-full lg:w-1/2 bg-white p-8">
-        <div className="w-full max-w-sm">
-          <h1 className="text-2xl font-bold text-gray-900 mb-6">Sign Up</h1>
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="first_name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                First Name
-              </label>
-              <div className="mt-1">
-                <input
-                  id="first_name"
-                  name="first_name"
-                  type="text"
-                  autoComplete="off"
-                  required
-                  value={inputs.first_name || ""}
-                  onChange={handleChange}
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                htmlFor="last_name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Last Name
-              </label>
-              <div className="mt-1">
-                <input
-                  id="last_name"
-                  name="last_name"
-                  type="text"
-                  autoComplete="off"
-                  required
-                  value={inputs.last_name || ""}
-                  onChange={handleChange}
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <div className="mt-1">
-                <input
-                  id="email"
-                  name="email"
-                  type="text"
-                  autoComplete="off"
-                  required
-                  value={inputs.email || ""}
-                  onChange={handleChange}
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs((values) => ({ ...values, [name]: value }));
+    };
 
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <div className="mt-1">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="off"
-                  required
-                  value={inputs.password || ""}
-                  onChange={handleChange}
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
+    const { toast } = useToast();
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        const response = await fetch("/api/auth/register", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(inputs),
+        });
+        if (response.ok) {
+            toast({
+                title: "User created",
+                description: "User has been created successfully",
+            });
+            await signIn("credentials", {
+                email: inputs.email,
+                password: inputs.password,
+                callbackUrl: "/",
+            });
+        } else {
+            toast({
+                title: "Error",
+                description: "An error occurred trying to create the user",
+                variant: "destructive",
+            });
+        }
+    };
 
-            <div>
-              <button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border rounded-md shadow-sm text-sm font-medium text-white focus:ring-indigo-500 bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-cyan-300 hover:to-blue-400"
-              >
-                Register
-              </button>
+    return (
+        <div className="min-h-screen flex">
+            <div className="flex flex-col justify-center items-center w-full lg:w-1/2 bg-white p-8">
+                <div className="w-full max-w-sm">
+                    <h1 className="text-2xl font-bold text-gray-900 mb-6">
+                        Opret Dig
+                    </h1>
+                    <form className="space-y-6" onSubmit={handleSubmit}>
+                        <div>
+                            <label
+                                htmlFor="first_name"
+                                className="block text-sm font-medium text-gray-700"
+                            >
+                                Fornavn
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    id="first_name"
+                                    name="first_name"
+                                    type="text"
+                                    autoComplete="off"
+                                    required
+                                    value={inputs.first_name || ""}
+                                    onChange={handleChange}
+                                    className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="last_name"
+                                className="block text-sm font-medium text-gray-700"
+                            >
+                                Efternavn
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    id="last_name"
+                                    name="last_name"
+                                    type="text"
+                                    autoComplete="off"
+                                    required
+                                    value={inputs.last_name || ""}
+                                    onChange={handleChange}
+                                    className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="email"
+                                className="block text-sm font-medium text-gray-700"
+                            >
+                                Email
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="text"
+                                    autoComplete="off"
+                                    required
+                                    value={inputs.email || ""}
+                                    onChange={handleChange}
+                                    className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label
+                                htmlFor="password"
+                                className="block text-sm font-medium text-gray-700"
+                            >
+                                Adgangskode
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="off"
+                                    required
+                                    value={inputs.password || ""}
+                                    onChange={handleChange}
+                                    className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="w-full flex justify-center py-2 px-4 border rounded-md shadow-sm text-sm font-medium text-white focus:ring-indigo-500 bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-cyan-300 hover:to-blue-400"
+                            >
+                                Tilmeld
+                            </button>
+                        </div>
+                    </form>
+                    <div className="mt-6 flex items-center justify-center">
+                        <p className="text-gray-600 mr-2">
+                            Har du allerede en konto?
+                        </p>
+                        <Link
+                            href="/auth/signin"
+                            className="font-medium text-indigo-600 hover:text-indigo-500 underline"
+                        >
+                            Log Ind
+                        </Link>
+                    </div>
+                </div>
             </div>
-          </form>
-          <div className="mt-6 flex items-center justify-center">
-            <p className="text-gray-600 mr-2">Already have an account?</p>
-            <Link
-              href="/auth/signin"
-              className="font-medium text-indigo-600 hover:text-indigo-500 underline"
-            >
-              Log In
-            </Link>
-          </div>
+            <div className="hidden lg:flex lg:w-1/2 bg-gradient-to-r from-cyan-500 to-blue-500 text-white items-center justify-center">
+                <div className="text-center px-4">
+                    <Image
+                        src={bankIcon}
+                        alt="Finance"
+                        width={100}
+                        height={60}
+                        className="mx-auto mb-4"
+                    />
+                    <h2 className="text-2xl font-bold">
+                        Samlet økonomi, samlet overblik.
+                    </h2>
+                </div>
+            </div>
+            <div className="absolute -bottom-6 w-full">
+                <Image src={wave} alt="Wave SVG" layout="responsive" />
+            </div>
         </div>
-      </div>
-      <div className="hidden lg:flex lg:w-1/2 bg-blue-500 text-white items-center justify-center">
-        <div className="text-center px-4">
-          <Image
-            src="/images/GDP.jpg"
-            alt="Finance Dashboard"
-            className="mx-auto mb-4"
-            width={300}
-            height={200}
-          />
-          <h2 className="text-2xl font-bold">
-            Get All Your Finances At One Place.
-          </h2>
-        </div>
-      </div>
-      <Image
-        src="/wave.svg"
-        alt="Wave SVG"
-        className="mx-auto mt-8"
-        width={300}
-        height={100}
-      />
-    </div>
-  );
+    );
 }
--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -7,124 +7,128 @@ import bankIcon from "@/../public/bank-icon.png";
 import wave from "@/../public/wave.svg";
 
 type LoginInput = {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 };
 
 type PageProps = {
-  searchParams: { error?: string };
+    searchParams: { error?: string };
 };
 
 export default function LoginPage({ searchParams }: PageProps) {
-  const [inputs, setInputs] = useState<LoginInput>({
-    email: "",
-    password: "",
-  });
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs((values) => ({ ...values, [name]: value }));
-  };
-
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault();
-    await signIn("credentials", {
-      email: inputs.email,
-      password: inputs.password,
-      callbackUrl: "/",
+    const [inputs, setInputs] = useState<LoginInput>({
+        email: "",
+        password: "",
     });
-  };
-  return (
-    <div className="min-h-screen flex">
-      <div className="flex flex-col justify-center items-center w-full lg:w-1/2 bg-white p-8">
-        <div className="w-full max-w-sm">
-          <h1 className="text-2xl font-bold text-gray-900 mb-6">Log In</h1>
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <div className="mt-1">
-                <input
-                  id="email"
-                  name="email"
-                  type="text"
-                  autoComplete="off"
-                  required
-                  value={inputs.email || ""}
-                  onChange={handleChange}
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
 
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="off"
-                  required
-                  value={inputs.password || ""}
-                  onChange={handleChange}
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs((values) => ({ ...values, [name]: value }));
+    };
 
-            <div>
-              <button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border rounded-md shadow-sm text-sm font-medium text-white focus:ring-indigo-500 bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-cyan-300 hover:to-blue-400"
-              >
-                Log In
-              </button>
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+        await signIn("credentials", {
+            email: inputs.email,
+            password: inputs.password,
+            callbackUrl: "/",
+        });
+    };
+    return (
+        <div className="min-h-screen flex">
+            <div className="flex flex-col justify-center items-center w-full lg:w-1/2 bg-white p-8">
+                <div className="w-full max-w-sm">
+                    <h1 className="text-2xl font-bold text-gray-900 mb-6">
+                        Log Ind
+                    </h1>
+                    <form className="space-y-6" onSubmit={handleSubmit}>
+                        <div>
+                            <label
+                                htmlFor="email"
+                                className="block text-sm font-medium text-gray-700"
+                            >
+                                Email
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="text"
+                                    autoComplete="off"
+                                    required
+                                    value={inputs.email || ""}
+                                    onChange={handleChange}
+                                    className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label
+                                htmlFor="password"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                            >
+                                Adgangskode
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="off"
+                                    required
+                                    value={inputs.password || ""}
+                                    onChange={handleChange}
+                                    className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="w-full flex justify-center py-2 px-4 border rounded-md shadow-sm text-sm font-medium text-white focus:ring-indigo-500 bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-cyan-300 hover:to-blue-400"
+                            >
+                                Log Ind
+                            </button>
+                        </div>
+                        {searchParams.error && (
+                            <p className="text-red-600 text-center capitalize">
+                                Log ind failed.
+                            </p>
+                        )}
+                    </form>
+                    <div className="mt-6 flex items-center justify-center">
+                        <p className="text-gray-600 mr-2">
+                            Har du ikke en konto?
+                        </p>
+                        <Link
+                            href="/auth/register"
+                            className="font-medium text-indigo-600 hover:text-indigo-500 underline"
+                        >
+                            Opret Dig
+                        </Link>
+                    </div>
+                </div>
             </div>
-            {searchParams.error && (
-              <p className="text-red-600 text-center capitalize">
-                Login failed.
-              </p>
-            )}
-          </form>
-          <div className="mt-6 flex items-center justify-center">
-            <p className="text-gray-600 mr-2">Don't have an account?</p>
-            <Link
-              href="/auth/register"
-              className="font-medium text-indigo-600 hover:text-indigo-500 underline"
-            >
-              Sign Up
-            </Link>
-          </div>
+            <div className="hidden lg:flex lg:w-1/2 bg-gradient-to-r from-cyan-500 to-blue-500 text-white items-center justify-center">
+                <div className="text-center px-4">
+                    <Image
+                        src={bankIcon}
+                        alt="Finance"
+                        width={100}
+                        height={60}
+                        className="mx-auto mb-4"
+                    />
+                    <h2 className="text-2xl font-bold">
+                        Samlet økonomi, samlet overblik.
+                    </h2>
+                </div>
+            </div>
+            <div className="absolute -bottom-6 w-full">
+                <Image src={wave} alt="Wave SVG" layout="responsive" />
+            </div>
         </div>
-      </div>
-      <div className="hidden lg:flex lg:w-1/2 bg-gradient-to-r from-cyan-500 to-blue-500 text-white items-center justify-center">
-        <div className="text-center px-4">
-          <Image
-            src={bankIcon}
-            alt="Finance"
-            width={100}
-            height={60}
-            className="mx-auto mb-4"
-          />
-          <h2 className="text-2xl font-bold">
-            Get All Your Finances At One Place.
-          </h2>
-        </div>
-      </div>
-      <div className="absolute -bottom-6 w-full">
-        <Image src={wave} alt="Wave SVG" layout="responsive" />
-      </div>
-    </div>
-  );
+    );
 }
