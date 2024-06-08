06e5a6e1577a4cc6a44a0508d7401677b0eac788
Victorschimmell <Victorschimmell@gmail.com>
2024-06-09 00:54:24 +0100

    Optimized WIP
    Added optimization and scalability to the WIP page making it into a component taking in a pagename.

--- src/app/dashboard/rapporter/page.tsx
+++ src/app/dashboard/rapporter/page.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import { Ban } from 'lucide-react';
+import WorkInProgressPage from '@/components/client/WIP';
 
 type Props = {};
 
-const WorkInProgressPage: React.FC<Props> = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center">
-        <Ban className="w-16 h-16 text-yellow-600 mx-auto mb-4" />
-        <h1 className="text-3xl font-semibold text-yellow-600 mb-4">
-          Work In Progress
-        </h1>
-        <p className="text-gray-700 text-lg mb-4">
-          Rapporter er nuværende under arbejde.
-        </p>
-        <p className="text-gray-500">
-          Vend venligst tilbage senere eller kontakt support, hvis du har brug
-          for mere Information.
-        </p>
-      </div>
-    </div>
-  );
+const ReportsPage: React.FC<Props> = () => {
+  return <WorkInProgressPage pagename="Rapporter" />;
 };
 
-export default WorkInProgressPage;
+export default ReportsPage;
